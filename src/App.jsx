eb3f1b0c896b133286dd3f5c1a8a273033ddd96b
vaishnavigithub899 <vaishnavijha03aug@gmail.com>
2025-09-04 15:04:51 +0530

    Initial Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import './App.css'
-import { Home } from "./pages/Home";
-import { Pricing } from "./pages/Pricing";
-import { About } from "./pages/About";
-import { Learn } from "./pages/Learn";
-import { Navbar } from "./components/Navbar";
-import { Footer } from "./components/Footer";
+import { Home } from "./pages/Home.jsx";
+import { Pricing } from "./pages/Pricing.jsx";
+import { About } from "./pages/About.jsx";
+import { Learn } from "./pages/Learn.jsx";
+import { Navbar } from "./components/Navbar.jsx";
+import { Footer } from "./components/Footer.jsx";
 
 function App() {
   return (
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import { CloudCafe } from "../components/CloudCafe";
+import { CloudCafe } from "../components/CloudCafe.jsx";
 import { Pricing } from "./Pricing.jsx";
 import { Feedback } from "../components/Feedback.jsx";
 import { Speciality } from "../components/Speciality.jsx";
